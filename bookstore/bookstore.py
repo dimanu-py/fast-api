c989af2a2e543a177b659eb97e0b07441e6fd46b
dimanu-py <diegomtz126@gmail.com>
2024-03-19 13:01:43 +0100

    feat(bookstore): add books to the store

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -2,6 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+BOOKS = [
+    {"title": "One Hundred Years of Solitude", "author": "Gabriel Garcia Marquez", "category": "Novel"},
+    {"title": "The Catcher in the Rye", "author": "J.D. Salinger", "category": "Novel"},
+    {"title": "To Kill a Mockingbird", "author": "Harper Lee", "category": "Novel"},
+    {"title": "Beloved", "author": "Toni Morrison", "category": "Magical Realism"},
+    {"title": "Dune", "author": "Frank Herbert", "category": "Science Fiction"},
+]
+
 @app.get("/")
 async def root() -> dict[str, str]:
     return {"message": "Hello World"}
