7a5e0e8c1e4f85854e0d9ef91e3cae039c70c0ad
dimanu-py <diegomtz126@gmail.com>
2024-03-21 08:23:10 +0100

    refactor(schema): add Config class to schema to configure Swagger example
    By adding the json_schema_extra variable to the Config class we can set the default example that will be shown in the Swagger UI accessible at /docs endpoint

--- advanced_bookstore/request_schema.py
+++ advanced_bookstore/request_schema.py
@@ -11,3 +11,13 @@ class BookRequest(BaseModel):
     author: str = Field(min_length=1, max_length=100)
     category: str = Field(min_length=1, max_length=100)
     rating: int = Field(ge=1, le=5)
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "title": "Python Object-Oriented Programming",
+                "author": "Steven F. Lott",
+                "category": "Object-Oriented Programming",
+                "rating": 4
+            }
+        }
