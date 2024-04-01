505a4157c95b24499cb73425ef7e56c9155d910d
dimanu-py <diegomtz126@gmail.com>
2024-04-01 10:12:10 +0200

    feat(models): create Todos object
    This object represents the table todos we will have in the database. We can see that the table will have an id as primary key, a title, description, priority and completed columns
    
    The __tablename__ attribute allows sqlalchemy what table we are referring in our database.

--- todo_list/models.py
+++ todo_list/models.py
@@ -1,1 +1,12 @@
+from todo_list.database import Base
+from sqlalchemy import Column, Integer, String, Boolean
 
+
+class Todos(Base):
+    __tablename__ = "todos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    priority = Column(Integer, index=True)
+    completed = Column(Boolean, index=True)
