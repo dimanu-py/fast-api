8a6d92a37983232acc47a4fb66a856643c0d3420
dimanu-py <diegomtz126@gmail.com>
2024-04-01 10:01:21 +0200

    feat(database): create database session
    Each session local will have a database session. We bind the session to the engine we've created and set autocommit and autoflush to False to avoid making automatic transformations in the database

--- todo_list/database.py
+++ todo_list/database.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 
 DATABASE_URL = "sqlite:///todo_list/todos.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
