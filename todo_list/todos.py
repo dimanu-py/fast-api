f051a955c7bdeb4f343bf3e37fa54679da3ac766
dimanu-py <diegomtz126@gmail.com>
2024-06-07 20:28:27 +0200

    feat(todo-list): create GET endpoint to retrieve all todos in the database
    We've use dependency injection to tell the endpoint that before executing the endpoint itself, we need to execute the get_database function to open the connection to the database. Once the query has finished, the finally clause will be called and the connection will be close

--- todo_list/todos.py
+++ todo_list/todos.py
@@ -1,6 +1,8 @@
-from typing import Generator
+from typing import Generator, Annotated, Type
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
 from todo_list import models
 from todo_list.database import engine, SessionLocal
 
@@ -16,3 +18,8 @@ def get_database() -> Generator[SessionLocal, None, None]:
         yield db
     finally:
         db.close()
+
+
+@app.get("/todo")
+async def read_all_todos(db: Session = Depends(get_database)):
+    return db.query(models.Todos).all()
