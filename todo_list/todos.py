1e87a933fe763bc49fdfaf6c5a8a7fc1d93a902f
dimanu-py <diegomtz126@gmail.com>
2024-06-07 20:14:53 +0200

    feat(todo-list): create function to retrieve database sessions
    We will be opening the connection to the database only when is required, and close it once the operation is finished

--- todo_list/todos.py
+++ todo_list/todos.py
@@ -1,9 +1,18 @@
+from typing import Generator
+
 from fastapi import FastAPI
 from todo_list import models
-from todo_list.database import engine
-
+from todo_list.database import engine, Session
 
 app = FastAPI()
 
 
 models.Base.metadata.create_all(bind=engine)
+
+
+def get_database() -> Generator[Session]:
+    db = Session()
+    try:
+        yield db
+    finally:
+        db.close()
