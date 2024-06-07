ff6e952f2c46e2ebc0c179c96997a248cf7bcafb
dimanu-py <diegomtz126@gmail.com>
2024-06-07 21:06:58 +0200

    feat(todo-list): create GET endpoint to retrieve todo item given the id
    We've used previous concepts learned as the default status code to send when the response is successful or raise HTTPExceptions when the id is not found

--- todo_list/todos.py
+++ todo_list/todos.py
@@ -1,7 +1,8 @@
-from typing import Generator, Annotated, Type
+from typing import Generator
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
+from starlette import status
 
 from todo_list import models
 from todo_list.database import engine, SessionLocal
@@ -20,6 +21,15 @@ def get_database() -> Generator[SessionLocal, None, None]:
         db.close()
 
 
-@app.get("/todo")
+@app.get("/todo", status_code=status.HTTP_200_OK)
 async def read_all_todos(db: Session = Depends(get_database)):
     return db.query(models.Todos).all()
+
+
+@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+async def get_todo_by_id(todo_id: int, db: Session = Depends(get_database)):
+    selected_todo = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+    if selected_todo is not None:
+        return selected_todo
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Todo not with id {todo_id} not found")
