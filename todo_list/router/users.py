496a446b2719e1311e639e35526cfb748243eb98
dimanu-py <diegomtz126@gmail.com>
2024-06-08 15:31:34 +0200

    feat(users-router): create function to hash user password before inserting it in the database

--- todo_list/router/users.py
+++ todo_list/router/users.py
@@ -1,3 +1,4 @@
+import bcrypt
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from starlette import status
@@ -9,9 +10,22 @@ from todo_list.schemas.users import UserRequest
 router = APIRouter(prefix="/user", tags=["Users"])
 
 
+def hash_password(password: str) -> str:
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
+    return hashed_password.decode("utf-8")
+
+
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_user(user: UserRequest, db: Session = Depends(get_database)) -> None:
-    new_user = Users(**user.model_dump())
+    new_user = Users(
+        role=user.role,
+        username=user.username,
+        email=user.email,
+        first_name=user.first_name,
+        last_name=user.last_name,
+        hashed_password=hash_password(user.password),
+    )
 
     db.add(new_user)
     db.commit()
