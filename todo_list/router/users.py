87fa1ec49b06f49402b9ff58ff59d1910ed50c50
dimanu-py <diegomtz126@gmail.com>
2024-08-05 15:24:14 +0200

    feat(users-router): add endpoint to allow a logged user to change its password

--- todo_list/router/users.py
+++ todo_list/router/users.py
@@ -35,3 +35,14 @@ async def get_logged_user(user: AuthUser, db: Database) -> None:
     if user is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User authentication failed")
     return db.query(Users).filter_by(id=user.get("id")).first()
+
+
+@router.put("/change_password", status_code=status.HTTP_204_NO_CONTENT)
+async def change_password(user: AuthUser, password: str, db: Database) -> None:
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User authentication failed")
+
+    user = db.query(Users).filter_by(id=user.get("id")).first()
+    user.hashed_password = hash_password(password)
+    db.add(user)
+    db.commit()
