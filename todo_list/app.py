7f6553a359a858213bff798fe14d54a050422564
dimanu-py <diegomtz126@gmail.com>
2024-08-05 20:50:12 +0200

    refactor(app): remove import of engine and use database module import

--- todo_list/app.py
+++ todo_list/app.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI
 
 from todo_list import database
-from todo_list.database import engine
 from todo_list.router import users, todos, auth, admin
 
 app = FastAPI()
 
-database.Base.metadata.create_all(bind=engine)
+database.Base.metadata.create_all(bind=database.engine)
 
 app.include_router(users.router)
 app.include_router(todos.router)
